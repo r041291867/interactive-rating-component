9312275cfc56fb90d3d2c32b574416795009ed85
Jason Tai <r041291867@gmail.com>
2022-04-22 16:54:18 +0800

    using prettier

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-let current_score = '';
+let current_score = "";
 
 function handleClick(score) {
   if (current_score === score) return;
   if (current_score) {
-    setElementClassById(current_score, "")
+    setElementClassById(current_score, "");
   }
   setElementClassById(score, "orange");
   setRating(score);
 }
 
 function setRating(score) {
-  document.getElementById('score').innerHTML = score;
+  document.getElementById("score").innerHTML = score;
   current_score = score;
 }
 
 function setElementClassById(id, className) {
   let el = document.getElementById(id);
-  el.className = className
+  el.className = className;
 }
 
 function submitScore() {
-  if (!current_score) return
-  document.getElementById('rating').style.display = 'none';
-  document.getElementById('thank-you').style.display = 'block';
+  if (!current_score) return;
+  document.getElementById("rating").style.display = "none";
+  document.getElementById("thank-you").style.display = "block";
 }
